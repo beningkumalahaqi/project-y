a1cc5d5ccde33ec3a4bf4e0839efa53884a39996
Bening Kumala Haqi <114740995+beningkumalahaqi@users.noreply.github.com>
2024-02-08 08:58:43 +0000

    fix image in product detail page

--- client/src/app/product/[productId]/page.js
+++ client/src/app/product/[productId]/page.js
@@ -13,7 +13,7 @@ export default async function Page({params}) {
     <>
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
-        <Image src={product.image_link} alt={product.img_alt_text} className="max-w-sm rounded-lg shadow-2xl" width={1000} height={1000} />
+        <Image src={product.image_link} alt={product.img_alt_text} className="lg:max-w-lg rounded-lg shadow-2xl max-w-52" width={500} height={500} />
         <div>
           <h1 className="text-5xl font-bold">{product.title}</h1>
           <p className="py-6">{product.description}</p>
